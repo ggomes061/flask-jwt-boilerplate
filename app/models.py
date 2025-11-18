7c5b5f0411a3dd9576b5e0606dd3f21766d410ff
ggomes061 <ggomes351@proton.me>
2025-11-17 22:43:31 -0300

    fix strin to string

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Strin(80), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), default="user")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
