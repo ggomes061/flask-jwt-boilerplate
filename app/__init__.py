4bdcd964ea573f35e525b50e335a436241f4dbe4
ggomes061 <ggomes351@proton.me>
2025-11-17 22:31:00 -0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -13,15 +13,23 @@ jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
+
+    default_db = "sqlite:///tmp/app.db"
+
     app.config.from_mapping(
         SECRET_KEY=os.getenv("SECRET_KEY", "dev-secret"),
-        SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", "sqlite://app.db"),
-        JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY", "jwt-super-secret")
+        SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL", default_db),
+        JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY", "jwt-super-secret"),
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
+
+    with app.app_context():
+        from . import models
+        db.create_all()
 
     from .routes import api
     app.register_blueprint(api)
